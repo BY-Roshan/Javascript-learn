039b6860d32683a175a9aac5fd9f268e043e7217
Roshan Patel <evercoindia@gmail.com>
2026-02-22 03:10:39 +0000

    learn-variables

---
+++ BASIC_01/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 23232;//we can't cange tis value any where because it is a constant variable
+var accountName = "Roshan";
+let accountState = "odisha";
+accountGmail = "hy@gmail.com";
+
+console.log(accountName);
+console.table([accountId, accountName, accountState, accountGmail]);
