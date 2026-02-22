cfc6dcd17bb95f9adb5461458d637b2b41c800cb
Roshan Patel <evercoindia@gmail.com>
2026-02-22 03:39:45 +0000

    02_dataTypes

---
+++ BASIC_01/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+//alert("Hello World!");// it only works in browser, not in node.js
+
+// Data types
+// 1. String
+let name = "mohit";
+
+// 2. Number
+let age = 22;
+
+// 3. Boolean
+let isAdult = true;
+
+// 4. Null
+let address = null;// null is used for empty value, it is an assignment value, it can be assigned to a variable as a representation of no value
+
+// 5. Undefined
+let phoneNumber;
+
+// 6. Symbol
+let uniqueId = Symbol("id");
+
+// Displaying the data types
+console.log("Name:", name);
+console.log("Age:", age);
