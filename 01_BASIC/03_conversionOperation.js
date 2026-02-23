a651a721b3b10fad029d831d0142121f09f9f37b
Roshan Patel <evercoindia@gmail.com>
2026-02-23 14:27:36 +0000

    03_conversionOperation.js

---
+++ 01_BASIC/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "34"
+
+console.log(typeof score);
+console.log(typeof (score));//Both ore same 
+
+let ValueInNumber = Number(score);//Now string is converted to number
+
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let IsLoggedIn = 1 ;
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+
+//1 => true
+//0 => false
+// "" => false
+// "Roshan" => true
+
+let StringNumber = String(StringNumber)
+console.log(StringNumber)
